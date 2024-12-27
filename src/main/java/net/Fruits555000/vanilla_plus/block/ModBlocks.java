969ac3800c4a3e265392957f3204af22af1b246e
Fruits555000 <robert.hirst3636@gmail.com>
2024-12-27 11:13:42 +1100

    added items

--- src/main/java/net/Fruits555000/vanilla_plus/block/ModBlocks.java
+++ src/main/java/net/Fruits555000/vanilla_plus/block/ModBlocks.java
@@ -1,4 +1,8 @@
 package net.Fruits555000.vanilla_plus.block;
 
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+
 public class ModBlocks {
+    public static final DeferredRegister<Block>
 }
